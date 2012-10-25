042fa744fa1d2f87737d21a3a78f1db1101afe0a
rodob <contact@rodolfo.no>
2012-10-26 00:17:25 +0200

    Added ModelAndView method

--- src/main/java/no/rodob/springapp/SpringAppController.java
+++ src/main/java/no/rodob/springapp/SpringAppController.java
@@ -2,13 +2,24 @@ package no.rodob.springapp;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class SpringAppController {
 
-    @RequestMapping
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String home() {
-        System.out.print("this is home!");
-        return "/WEB-INF/jsp/home.jsp";
+        System.out.println("this is home!");
+        return "home";
+    }
+
+    @RequestMapping(value = "/give-me-a-list", method = RequestMethod.GET)
+    public ModelAndView listOfStrings() {
+        final List<String> items = Arrays.asList("first", "second", "third");
+        return new ModelAndView("modelAndView", "items", items);
     }
 }
